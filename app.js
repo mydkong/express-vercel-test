d0c0f4347c9894b6fb0c6f114309bf8e06be2ad6
mydkong <contact.donkeykong@protonmail.com>
2026-01-01 14:21:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
-app.use(express.static('src'));
-app.get('/', (req, res) => {
-    res.sendFile(path.join('src', req.url.substr(1)))
+//app.use(express.static('src'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', req.url.substr(1)))
 })
 
 module.exports = app;
