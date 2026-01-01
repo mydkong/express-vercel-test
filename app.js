5722321d9d7caaf4e7e0e7be8e3418a5f22d2f97
mydkong <contact.donkeykong@protonmail.com>
2026-01-01 12:44:26 +0100

    Serve static files from the 'src' directory
    Add static file serving for the 'src' directory

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
-app.use('/', routes);
+app.use(express.static('src'));
 
 module.exports = app;
