dbd07897d4c6489e4f7265ad7c0f930cdccca707
mydkong <contact.donkeykong@protonmail.com>
2026-01-01 13:54:52 +0100

    Add route to serve static files from src directory

--- app.js
+++ app.js
@@ -8,5 +8,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
 app.use(express.static('src'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join('src', req.url.substr(1)))
+})
 
 module.exports = app;
