abbad6adc5ad145152e6e569d8ebebabb0179ae6
mydkong <contact.donkeykong@protonmail.com>
2026-01-01 15:22:07 +0100

    Add URL / only support

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
 //app.use(express.static('src'));
 app.get((req, res) => {
+    let url = res.url
+    if (url.test(/\/$/)) {
+        url += 'index.html'
+    }
     //res.sendFile(path.join(__dirname, 'src', req.url.substr(1)))
-    res.json(path.join(__dirname, 'src', req.url.substr(1)))
+    res.json(path.join(__dirname, 'src', url.substr(1)))
 })
 
 module.exports = app;
