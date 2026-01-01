d099304f0e841c054daf7d2bc8828e6ce0e6e4c9
mydkong <contact.donkeykong@protonmail.com>
2026-01-01 15:51:43 +0100

    Retest logging :(

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
-//app.use(express.static('src'));
+
 app.get((req, res) => {
     let url = res.url
     if (url.test(/\/$/)) {
         url += 'index.html'
     }
+    url = url.substring(1)
     //res.sendFile(path.join(__dirname, 'src', req.url.substr(1)))
-    res.json(path.join(__dirname, 'src', url.substr(1)))
+    res.json(url)
 })
 
 module.exports = app;
