ca8144158c57eb5d399027d5e1080f6c22813af4
mydkong <contact.donkeykong@protonmail.com>
2026-01-01 14:56:55 +0100

    Correction in requests handler

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
 //app.use(express.static('src'));
-app.get('/*', (req, res) => {
+app.get((req, res) => {
     res.sendFile(path.join(__dirname, 'src', req.url.substr(1)))
 })
 
